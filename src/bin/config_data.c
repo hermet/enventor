hermet/enventor

commit bd0d0008deb431ed0b5ff820b98e226720d9cc18

config - fix the side effect that paths buf weren't intialized.
it's caused by 5b69d97f8c20c8cbe7e8b75e97cb0c010be56430.

src/bin/config_data.c
@@ -82,6 +82,23 @@ config_save(config_data *cd)
    eet_close(ef);
 }
 
+static Eina_Strbuf *
+config_paths_buf_load_set(Eina_List *paths)
+{
+   Eina_List *l;
+   const char *s;
+
+   Eina_Strbuf *buf = eina_strbuf_new();
+
+   EINA_LIST_FOREACH(paths, l, s)
+     {
+        eina_strbuf_append(buf, " -fd ");
+        eina_strbuf_append(buf, s);
+     }
+
+   return buf;
+}
+
 static config_data *
 config_load()
 {
@@ -102,16 +119,6 @@ config_load()
    if (!cd)
      {
         cd = calloc(1, sizeof(config_data));
-        g_cd = cd;
-
-        sprintf(buf, "%s/images", elm_app_data_dir_get());
-        config_edc_img_path_set(buf);
-        sprintf(buf, "%s/sounds", elm_app_data_dir_get());
-        config_edc_snd_path_set(buf);
-        sprintf(buf, "%s/fonts", elm_app_data_dir_get());
-        config_edc_fnt_path_set(buf);
-        sprintf(buf, "%s/data", elm_app_data_dir_get());
-        config_edc_data_path_set(buf);
 
         cd->font_size = 1.0f;
         cd->view_scale = 1;
@@ -122,6 +129,37 @@ config_load()
         cd->auto_indent = EINA_TRUE;
         cd->hotkeys = EINA_TRUE;
      }
+
+   g_cd = cd;
+
+   if (!cd->edc_img_path_list)
+     {
+        sprintf(buf, "%s/images", elm_app_data_dir_get());
+        config_edc_img_path_set(buf);
+     }
+   else cd->edc_img_path_buf = config_paths_buf_set(cd->edc_img_path_list);
+
+   if (!cd->edc_snd_path_list)
+     {
+        sprintf(buf, "%s/sounds", elm_app_data_dir_get());
+        config_edc_snd_path_set(buf);
+     }
+   else cd->edc_snd_path_buf = config_paths_buf_set(cd->edc_snd_path_list);
+
+   if (!cd->edc_fnt_path_list)
+     {
+        sprintf(buf, "%s/fonts", elm_app_data_dir_get());
+        config_edc_fnt_path_set(buf);
+     }
+   else cd->edc_fnt_path_buf = config_paths_buf_set(cd->edc_fnt_path_list);
+
+   if (!cd->edc_data_path_list)
+     {
+        sprintf(buf, "%s/data", elm_app_data_dir_get());
+        config_edc_data_path_set(buf);
+     }
+   else cd->edc_data_path_buf = config_paths_buf_set(cd->edc_data_path_list);
+
    return cd;
 }
 

commit bb22983b4b49c23b43be7dc2b0f18c6a3459e508

config - fix the side effect again.
caused by bd0d0008deb431ed0b5ff820b98e226720d9cc18.

the prefix should not fixed and function name is incorrect.

fixed.

src/bin/config_data.c
@@ -83,7 +83,7 @@ config_save(config_data *cd)
 }
 
 static Eina_Strbuf *
-config_paths_buf_load_set(Eina_List *paths)
+config_paths_buf_set(Eina_List *paths, const char *prefix)
 {
    Eina_List *l;
    const char *s;
@@ -92,7 +92,7 @@ config_paths_buf_load_set(Eina_List *paths)
 
    EINA_LIST_FOREACH(paths, l, s)
      {
-        eina_strbuf_append(buf, " -fd ");
+        eina_strbuf_append(buf, prefix);
         eina_strbuf_append(buf, s);
      }
 
@@ -137,28 +137,32 @@ config_load()
         sprintf(buf, "%s/images", elm_app_data_dir_get());
         config_edc_img_path_set(buf);
      }
-   else cd->edc_img_path_buf = config_paths_buf_set(cd->edc_img_path_list);
+   else cd->edc_img_path_buf =
+     config_paths_buf_set(cd->edc_img_path_list, " -id ");
 
    if (!cd->edc_snd_path_list)
      {
         sprintf(buf, "%s/sounds", elm_app_data_dir_get());
         config_edc_snd_path_set(buf);
      }
-   else cd->edc_snd_path_buf = config_paths_buf_set(cd->edc_snd_path_list);
+   else cd->edc_snd_path_buf =
+     config_paths_buf_set(cd->edc_snd_path_list, " -sd ");
 
    if (!cd->edc_fnt_path_list)
      {
         sprintf(buf, "%s/fonts", elm_app_data_dir_get());
         config_edc_fnt_path_set(buf);
      }
-   else cd->edc_fnt_path_buf = config_paths_buf_set(cd->edc_fnt_path_list);
+   else cd->edc_fnt_path_buf =
+     config_paths_buf_set(cd->edc_fnt_path_list, " -fd ");
 
    if (!cd->edc_data_path_list)
      {
         sprintf(buf, "%s/data", elm_app_data_dir_get());
         config_edc_data_path_set(buf);
      }
-   else cd->edc_data_path_buf = config_paths_buf_set(cd->edc_data_path_list);
+   else cd->edc_data_path_buf =
+     config_paths_buf_set(cd->edc_data_path_list, " -dd ");
 
    return cd;
 }