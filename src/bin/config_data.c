hermet/enventor

commit 5b69d97f8c20c8cbe7e8b75e97cb0c010be56430

config - use the EET_DATA_DESCRIPTOR_ADD_LIST_STRING for paths i/o
it's better simple and easy. cool!

src/bin/config_data.c
@@ -52,25 +52,6 @@ config_edj_path_update(config_data *cd)
 }
 
 static void
-edc_paths_write(Eet_File *ef, const char *key, Eina_List *paths,
-              Eina_Strbuf *strbuf)
-{
-   Eina_List *l;
-   char *path;
-
-   eina_strbuf_reset(strbuf);
-
-   EINA_LIST_FOREACH(paths, l, path)
-     {
-        eina_strbuf_append(strbuf, path);
-        eina_strbuf_append(strbuf, ";");
-     }
-
-   const char *str = eina_strbuf_string_get(strbuf);
-   eet_write(ef, key, str, strlen(str) + 1, 0);
-}
-
-static void
 config_save(config_data *cd)
 {
    char buf[PATH_MAX];
@@ -97,15 +78,7 @@ config_save(config_data *cd)
         return;
      }
 
-   //TODO: Use Eet Descriptor if the attributes are getting bigger and bigger
    eet_data_write(ef, edd_base, "config", cd, 1);
-   Eina_Strbuf *strbuf = eina_strbuf_new();
-   edc_paths_write(ef, "imgpath", cd->edc_img_path_list, strbuf);
-   edc_paths_write(ef, "sndpath", cd->edc_snd_path_list, strbuf);
-   edc_paths_write(ef, "fntpath", cd->edc_fnt_path_list, strbuf);
-   edc_paths_write(ef, "datpath", cd->edc_data_path_list, strbuf);
-   eina_strbuf_free(strbuf);
-
    eet_close(ef);
 }
 
@@ -149,36 +122,6 @@ config_load()
         cd->auto_indent = EINA_TRUE;
         cd->hotkeys = EINA_TRUE;
      }
-   else
-     {
-        g_cd = cd;
-        int size;
-        char *ret;
-        ret = eet_read(ef, "imgpath", &size);
-        if (size > 0)
-          {
-             config_edc_img_path_set(ret);
-             free(ret);
-          }
-        ret = eet_read(ef, "sndpath", &size);
-        if (size > 0)
-          {
-             config_edc_snd_path_set(ret);
-             free(ret);
-          }
-        ret = eet_read(ef, "fntpath", &size);
-        if (size > 0)
-          {
-             config_edc_fnt_path_set(ret);
-             free(ret);
-          }
-        ret = eet_read(ef, "datpath", &size);
-        if (size > 0)
-          {
-             config_edc_data_path_set(ret);
-             free(ret);
-          }
-     }
    return cd;
 }
 
@@ -189,6 +132,16 @@ eddc_init()
    eet_eina_stream_data_descriptor_class_set(&eddc, sizeof(eddc),
                                              "config", sizeof(config_data));
    edd_base = eet_data_descriptor_stream_new(&eddc);
+
+   EET_DATA_DESCRIPTOR_ADD_LIST_STRING(edd_base, config_data,
+                                       "edc_img_path_list", edc_img_path_list);
+   EET_DATA_DESCRIPTOR_ADD_LIST_STRING(edd_base, config_data,
+                                       "edc_snd_path_list", edc_snd_path_list);
+   EET_DATA_DESCRIPTOR_ADD_LIST_STRING(edd_base, config_data,
+                                       "edc_fnt_path_list", edc_fnt_path_list);
+   EET_DATA_DESCRIPTOR_ADD_LIST_STRING(edd_base, config_data,
+                                       "edc_data_path_list",
+                                       edc_data_path_list);
    EET_DATA_DESCRIPTOR_ADD_BASIC(edd_base, config_data, "font_size", font_size,
                                  EET_T_FLOAT);
    EET_DATA_DESCRIPTOR_ADD_BASIC(edd_base, config_data, "view_scale",
@@ -223,6 +176,7 @@ config_init(const char *edc_path, const char *edc_img_path,
    eddc_init();
 
    config_data *cd = config_load();
+   g_cd = cd;
 
    if (edc_path[0]) config_edc_path_set(edc_path);
    if (edc_img_path[0]) config_edc_img_path_set(edc_img_path);

commit bd0d0008deb431ed0b5ff820b98e226720d9cc18

config - fix the side effect that paths buf weren't intialized.
it's caused by 5b69d97f8c20c8cbe7e8b75e97cb0c010be56430.

src/bin/config_data.c
@@ -82,6 +82,23 @@ config_save(config_data *cd)
    eet_close(ef);
 }
 
+static Eina_Strbuf *
+config_paths_buf_load_set(Eina_List *paths)
+{
+   Eina_List *l;
+   const char *s;
+
+   Eina_Strbuf *buf = eina_strbuf_new();
+
+   EINA_LIST_FOREACH(paths, l, s)
+     {
+        eina_strbuf_append(buf, " -fd ");
+        eina_strbuf_append(buf, s);
+     }
+
+   return buf;
+}
+
 static config_data *
 config_load()
 {
@@ -102,16 +119,6 @@ config_load()
    if (!cd)
      {
         cd = calloc(1, sizeof(config_data));
-        g_cd = cd;
-
-        sprintf(buf, "%s/images", elm_app_data_dir_get());
-        config_edc_img_path_set(buf);
-        sprintf(buf, "%s/sounds", elm_app_data_dir_get());
-        config_edc_snd_path_set(buf);
-        sprintf(buf, "%s/fonts", elm_app_data_dir_get());
-        config_edc_fnt_path_set(buf);
-        sprintf(buf, "%s/data", elm_app_data_dir_get());
-        config_edc_data_path_set(buf);
 
         cd->font_size = 1.0f;
         cd->view_scale = 1;
@@ -122,6 +129,37 @@ config_load()
         cd->auto_indent = EINA_TRUE;
         cd->hotkeys = EINA_TRUE;
      }
+
+   g_cd = cd;
+
+   if (!cd->edc_img_path_list)
+     {
+        sprintf(buf, "%s/images", elm_app_data_dir_get());
+        config_edc_img_path_set(buf);
+     }
+   else cd->edc_img_path_buf = config_paths_buf_set(cd->edc_img_path_list);
+
+   if (!cd->edc_snd_path_list)
+     {
+        sprintf(buf, "%s/sounds", elm_app_data_dir_get());
+        config_edc_snd_path_set(buf);
+     }
+   else cd->edc_snd_path_buf = config_paths_buf_set(cd->edc_snd_path_list);
+
+   if (!cd->edc_fnt_path_list)
+     {
+        sprintf(buf, "%s/fonts", elm_app_data_dir_get());
+        config_edc_fnt_path_set(buf);
+     }
+   else cd->edc_fnt_path_buf = config_paths_buf_set(cd->edc_fnt_path_list);
+
+   if (!cd->edc_data_path_list)
+     {
+        sprintf(buf, "%s/data", elm_app_data_dir_get());
+        config_edc_data_path_set(buf);
+     }
+   else cd->edc_data_path_buf = config_paths_buf_set(cd->edc_data_path_list);
+
    return cd;
 }
 