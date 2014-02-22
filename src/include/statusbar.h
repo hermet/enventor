cb7c6f085f07879b45d46abd46c915a848200841
ChunEon Park <hermet@hermet.pe.kr>
2014-02-22 13:53:26 +0900

    ctxpopup - adjust button size in the slider.

--- src/include/statusbar.h
+++ src/include/statusbar.h
@@ -5,5 +5,5 @@ void stats_cursor_pos_update(Evas_Coord x, Evas_Coord y, float rel_x, float rel_
 void stats_info_msg_update(const char *msg);
 void stats_line_num_update(int cur_line, int max_line);
 Evas_Object *stats_obj_get();
-void stats_edc_group_set(const char *group_name);
+void stats_edc_group_update(const char *group_name);
 Eina_Stringshare *stats_group_name_get();
