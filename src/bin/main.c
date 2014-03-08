hermet/enventor

commit dc3ce7be3f039aacc76140dda514b09edabc7761

hotkeys - redesign ui

src/bin/main.c
@@ -78,21 +78,6 @@ main_key_up_cb(void *data, int type EINA_UNUSED, void *ev)
 }
 
 static void
-part_highlight_toggle(app_data *ad, Eina_Bool msg)
-{
-   Eina_Bool highlight = config_part_highlight_get();
-   if (highlight) edit_view_sync(ad->ed);
-   else view_part_highlight_set(VIEW_DATA, NULL);
-
-   if (!msg) return;
-
-   if (highlight)
-     stats_info_msg_update("Part Highlighting Enabled.");
-   else
-     stats_info_msg_update("Part Highlighting Disabled.");
-}
-
-static void
 auto_indentation_toggle()
 {
    Eina_Bool toggle = !config_auto_indent_get();
@@ -168,17 +153,11 @@ ctrl_func(app_data *ad, const char *key)
         search_open();
         return ECORE_CALLBACK_DONE;
      }
-   //Template Code
-   if (!strcmp(key, "t") || !strcmp(key, "T"))
-     {
-        edit_template_insert(ad->ed);
-        return ECORE_CALLBACK_DONE;
-     }
    //Part Highlight
-   if (!strcmp(key, "h") || !strcmp(key, "H"))
+   if (!strcmp(key, "h") || !strcmp(key, "h"))
      {
-        config_part_highlight_set(!config_part_highlight_get());
-        part_highlight_toggle(ad, EINA_TRUE);
+       config_part_highlight_set(!config_part_highlight_get());
+       edit_part_highlight_toggle(ad->ed, EINA_TRUE);
         return ECORE_CALLBACK_DONE;
      }
    //Swallow Dummy Object
@@ -186,6 +165,12 @@ ctrl_func(app_data *ad, const char *key)
      {
         config_dummy_swallow_set(!config_dummy_swallow_get());
         view_dummy_toggle(VIEW_DATA, EINA_TRUE);
+        return ECORE_CALLBACK_DONE;
+     }
+   //Template Code
+   if (!strcmp(key, "t") || !strcmp(key, "T"))
+     {
+        edit_template_insert(ad->ed);
         return ECORE_CALLBACK_DONE;
      }
    //Full Edit View
@@ -289,6 +274,20 @@ main_key_down_cb(void *data, int type EINA_UNUSED, void *ev)
         menu_edc_load();
         return ECORE_CALLBACK_DONE;
      }
+   //Part Highlight
+   if (!strcmp(event->key, "F5"))
+     {
+        config_part_highlight_set(!config_part_highlight_get());
+        edit_part_highlight_toggle(ad->ed, EINA_TRUE);
+        return ECORE_CALLBACK_DONE;
+     }
+   //Swallow Dummy Object
+   if (!strcmp(event->key, "F6"))
+     {
+        config_dummy_swallow_set(!config_dummy_swallow_get());
+        view_dummy_toggle(VIEW_DATA, EINA_TRUE);
+        return ECORE_CALLBACK_DONE;
+     }
    //Line Number
    if (!strcmp(event->key, "F5"))
      {
@@ -388,7 +387,7 @@ config_update_cb(void *data)
    edit_font_size_update(ad->ed, EINA_FALSE);
 
    base_statusbar_toggle();
-   part_highlight_toggle(ad, EINA_FALSE);
+   edit_part_highlight_toggle(ad->ed, EINA_TRUE);
    view_dummy_toggle(VIEW_DATA, EINA_FALSE);
 
    //previous build was failed, Need to rebuild then reload the edj.

README
@@ -36,27 +36,20 @@ Once you have met requirements, compiling and installing is simple:
 [Short Cut Keys]
  
 Esc = Open/Close Menu
-F1 = Help
+F1 = About
 F2 = New
 F3 = Save
 F4 = Load
 F5 = Show/Hide Line Number
 F6 = Show/Hide Status
 F12 = Setting
+
 Ctrl+S = Quick Save + Compile EDC
-Ctrl+A = Select Text All
-Ctrl+Double Click = Select a word
-Ctrl+C = Copy Selected Text
-Ctrl+V = Paste Copied Text
-Ctrl+X = Cut Selected Text
-Ctrl+F = Find/Replace
 Ctrl+I = On/Off Auto Indentation
-Ctrl+Home = Go to the Top line
-Ctrl+End = Go to the Bottom line
-Ctrl+H = Part Highlighting
+Ctrl+W = Show/Hide Dummy Swallow
+Ctrl+H = On/Off Part Highlighting
 Ctrl+, = Full Edit View / Split View
 Ctrl+. = Full Edje View / Split View
-Ctrl+W = Show/Hide Dummy Swallow Object
 Ctrl++ = Font Size Up
 Ctrl+- = Font Size Down
 Ctrl+T = Insert Defaut Template Code
@@ -68,7 +61,17 @@ Ctrl+Shift+R = Insert Part Template Code: Rectangle
 Ctrl+Shift+T = Insert Part Template Code: Text
 Ctrl+Shift+S = Insert Part Template Code: Spacer
 Ctrl+Shift+W = Insert Part Template Code: Swallow
- 
+
+Ctrl+A = Select Text All
+Ctrl+Double Click = Select a word
+Ctrl+C = Copy Selected Text
+Ctrl+V = Paste Copied Text
+Ctrl+X = Cut Selected Text
+Ctrl+F = Find/Replace
+Ctrl+Home = Go to the Top line
+Ctrl+End = Go to the Bottom line
+
+
 [Command Line Usage]
  
 enventor --help

data/images/Makefile.am
@@ -1,6 +1,15 @@
 MAINTAINERCLEANFILES = Makefile.in
 
 filesdir = $(datadir)/$(PACKAGE)/images
-files_DATA = logo.png
+files_DATA = logo.png \
+             about.png \
+             new.png \
+             file.png \
+             highlight.png \
+             swallow.png \
+             status.png \
+             lines.png \
+             find.png \
+             setting.png
 
 EXTRA_DIST = $(files_DATA)

commit c8e85f3a68d7b9d3ac91afafb9a5cfe5821aca52

f5/f6 short cut keys had multiple functions.
caused by dc3ce7be3f039aacc76140dda514b09edabc7761

src/bin/main.c
@@ -274,20 +274,6 @@ main_key_down_cb(void *data, int type EINA_UNUSED, void *ev)
         menu_edc_load();
         return ECORE_CALLBACK_DONE;
      }
-   //Part Highlight
-   if (!strcmp(event->key, "F5"))
-     {
-        config_part_highlight_set(!config_part_highlight_get());
-        edit_part_highlight_toggle(ad->ed, EINA_TRUE);
-        return ECORE_CALLBACK_DONE;
-     }
-   //Swallow Dummy Object
-   if (!strcmp(event->key, "F6"))
-     {
-        config_dummy_swallow_set(!config_dummy_swallow_get());
-        view_dummy_toggle(VIEW_DATA, EINA_TRUE);
-        return ECORE_CALLBACK_DONE;
-     }
    //Line Number
    if (!strcmp(event->key, "F5"))
      {