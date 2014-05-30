f559e674021dc83a683c207af39b306116aac03f
ChunEon Park <hermet@hermet.pe.kr>
2014-05-30 19:53:09 +0900

    theme - removed panes buttons

--- src/bin/panes.c
+++ src/bin/panes.c
@@ -13,8 +13,6 @@ const char *PANES_DATA = "_panes_data";
 typedef struct _panes_data
 {
    Evas_Object *panes;
-   Evas_Object *left_arrow;
-   Evas_Object *right_arrow;
    Panes_State state;
 
    double origin;
@@ -66,55 +64,17 @@ panes_full_view_cancel(panes_data *pd)
    pd->state = PANES_SPLIT_VIEW;
 }
 
-static void
-left_clicked_cb(void *data, Evas_Object *obj, void *event_info EINA_UNUSED)
+void
+panes_full_view_right()
 {
    const double TRANSIT_TIME = 0.25;
 
-   panes_data *pd = data;
-
-   //Revert state if the current state is full view left already.
-   if (pd->state == PANES_FULL_VIEW_LEFT)
-     {
-        panes_full_view_cancel(pd);
-        Evas_Object *left_arrow_img = elm_object_content_get(obj);
-        elm_image_file_set(left_arrow_img, EDJE_PATH, "panes_left_arrow");
-        return;
-     }
-
-   double origin = elm_panes_content_right_size_get(pd->panes);
-   if (origin == 1.0) return;
-
-   pd->origin = origin;
-   pd->delta = 1.0 - pd->origin;
-
-   Elm_Transit *transit = elm_transit_add();
-   elm_transit_effect_add(transit, transit_op, pd, NULL);
-   elm_transit_tween_mode_set(transit, ELM_TRANSIT_TWEEN_MODE_DECELERATE);
-   elm_transit_duration_set(transit, TRANSIT_TIME);
-   elm_transit_go(transit);
-
-   pd->state = PANES_FULL_VIEW_LEFT;
-   Evas_Object *right_arrow_img = elm_object_content_get(pd->right_arrow);
-   elm_image_file_set(right_arrow_img, EDJE_PATH, "panes_right_arrow");
-
-   Evas_Object *left_arrow_img = elm_object_content_get(obj);
-   elm_image_file_set(left_arrow_img, EDJE_PATH, "panes_recover_arrow");
-}
-
-static void
-right_clicked_cb(void *data, Evas_Object *obj, void *event_info EINA_UNUSED)
-{
-   const double TRANSIT_TIME = 0.25;
-
-   panes_data *pd = data;
+   panes_data *pd = g_pd;
 
    //Revert state if the current state is full view left already.
    if (pd->state == PANES_FULL_VIEW_RIGHT)
      {
         panes_full_view_cancel(pd);
-        Evas_Object *right_arrow_img = elm_object_content_get(obj);
-        elm_image_file_set(right_arrow_img, EDJE_PATH, "panes_right_arrow");
         return;
      }
 
@@ -131,25 +91,36 @@ right_clicked_cb(void *data, Evas_Object *obj, void *event_info EINA_UNUSED)
    elm_transit_go(transit);
 
    pd->state = PANES_FULL_VIEW_RIGHT;
-   Evas_Object *left_arrow_img = elm_object_content_get(pd->left_arrow);
-   elm_image_file_set(left_arrow_img, EDJE_PATH, "panes_left_arrow");
-
-   Evas_Object *right_arrow_img = elm_object_content_get(obj);
-   elm_image_file_set(right_arrow_img, EDJE_PATH, "panes_recover_arrow");
-}
-
-void
-panes_full_view_right()
-{
-   panes_data *pd = g_pd;
-   right_clicked_cb(pd, pd->right_arrow, NULL);
 }
 
 void
 panes_full_view_left()
 {
+   const double TRANSIT_TIME = 0.25;
+
    panes_data *pd = g_pd;
-   left_clicked_cb(pd, pd->left_arrow, NULL);
+
+   //Revert state if the current state is full view left already.
+   if (pd->state == PANES_FULL_VIEW_LEFT)
+     {
+        panes_full_view_cancel(pd);
+        return;
+     }
+
+   double origin = elm_panes_content_right_size_get(pd->panes);
+   if (origin == 1.0) return;
+
+   pd->origin = origin;
+   pd->delta = 1.0 - pd->origin;
+
+   Elm_Transit *transit = elm_transit_add();
+   elm_transit_effect_add(transit, transit_op, pd, NULL);
+   elm_transit_tween_mode_set(transit, ELM_TRANSIT_TWEEN_MODE_DECELERATE);
+   elm_transit_duration_set(transit, TRANSIT_TIME);
+   elm_transit_go(transit);
+
+   pd->state = PANES_FULL_VIEW_LEFT;
+
 }
 
 void
@@ -183,31 +154,7 @@ panes_init(Evas_Object *parent)
                                   press_cb, NULL);
    evas_object_smart_callback_add(panes, "unpress",
                                   unpress_cb, NULL);
-   //Left Button
-   Evas_Object *left_arrow = elm_button_add(panes);
-   elm_object_focus_allow_set(left_arrow, EINA_FALSE);
-   evas_object_smart_callback_add(left_arrow, "clicked", left_clicked_cb, pd);
-   elm_object_part_content_set(panes, "elm.swallow.left_arrow", left_arrow);
-
-   //Left Arrow Image
-   img = elm_image_add(left_arrow);
-   elm_image_file_set(img, EDJE_PATH, "panes_left_arrow");
-   elm_object_content_set(left_arrow, img);
-
-   //Right Button
-   Evas_Object *right_arrow = elm_button_add(panes);
-   elm_object_focus_allow_set(right_arrow, EINA_FALSE);
-   evas_object_smart_callback_add(right_arrow, "clicked", right_clicked_cb, pd);
-   elm_object_part_content_set(panes, "elm.swallow.right_arrow", right_arrow);
-
-   //Right Arrow Image
-   img = elm_image_add(right_arrow);
-   elm_image_file_set(img, EDJE_PATH, "panes_right_arrow");
-   elm_object_content_set(right_arrow, img);
-
    pd->panes = panes;
-   pd->left_arrow = left_arrow;
-   pd->right_arrow = right_arrow;
    pd->state = PANES_SPLIT_VIEW;
 
    evas_object_data_set(panes, PANES_DATA, pd);
