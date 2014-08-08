5944f3d4c1b418547ce7ad0b408de0bcc4484c2f
ChunEon Park <hermet@hermet.pe.kr>
2014-08-08 17:32:46 +0900

    statusbar: improve layout.
    improve structure to be customizable.
    also, this removes the annoying padding at the end of the group name.

--- src/bin/statusbar.c
+++ src/bin/statusbar.c
@@ -16,10 +16,12 @@ stats_line_num_update(int cur_line, int max_line)
 {
    stats_data *sd = g_sd;
 
-   char buf[128];
-   snprintf(buf, sizeof(buf),
-            "<align=right>Line [<style=glow><color=#3399ff>%d</color></style>:<style=glow><color=#3399ff>%d</color></style>]</align>", cur_line, max_line);
-   elm_object_part_text_set(sd->layout, "elm.text.line", buf);
+   char buf[20];
+   snprintf(buf, sizeof(buf), "%d", cur_line);
+   elm_object_part_text_set(sd->layout, "elm.text.line_cur", buf);
+   snprintf(buf, sizeof(buf), "%d", max_line);
+   elm_object_part_text_set(sd->layout, "elm.text.line_max", buf);
+
    sd->cur_line = cur_line;
    sd->max_line = max_line;
 }
@@ -28,10 +30,7 @@ void
 stats_edc_group_update(Eina_Stringshare *group_name)
 {
    stats_data *sd = g_sd;
-
-   char buf[1028];
-   snprintf(buf, sizeof(buf), "<align=right>Group [<style=glow><color=#3399ff>%s</color></style></align>", group_name);
-   elm_object_part_text_set(sd->layout, "elm.text.group_name", buf);
+   elm_object_part_text_set(sd->layout, "elm.text.group_name", group_name);
    sd->group_name = eina_stringshare_add(group_name);
 }
 
@@ -89,20 +88,26 @@ stats_view_size_update(void)
    Evas_Coord w, h;
    config_view_size_get(&w, &h);
 
-   char buf[128];
-   snprintf(buf, sizeof(buf),
-            "Size [<style=glow><color=#3399ff>%d</color></style>x<style=glow><color=#3399ff>%d</color></style>]", w, h);
-
-   elm_object_part_text_set(sd->layout, "elm.text.view_size", buf);
+   char buf[10];
+   snprintf(buf, sizeof(buf), "%d", w);
+   elm_object_part_text_set(sd->layout, "elm.text.size_w", buf);
+   snprintf(buf, sizeof(buf), "%d", h);
+   elm_object_part_text_set(sd->layout, "elm.text.size_h", buf);
 }
 
 void
 stats_cursor_pos_update(Evas_Coord x, Evas_Coord y, float rel_x, float rel_y)
 {
    stats_data *sd = g_sd;
-   char buf[250];
-   snprintf(buf, sizeof(buf),
-            "Cursor [<style=glow><color=#3399ff>%d</color></style>,<style=glow><color=#3399ff>%d</color></style>] [<style=glow><color=#3399ff>%0.2f</color></style>,<style=glow><color=#3399ff>%0.2f</color></style>]", x, y, rel_x, rel_y);
 
-   elm_object_part_text_set(sd->layout, "elm.text.cur_pos", buf);
+   char buf[10];
+   snprintf(buf, sizeof(buf), "%d", x);
+   elm_object_part_text_set(sd->layout, "elm.text.cursor_pxx", buf);
+   snprintf(buf, sizeof(buf), "%d", y);
+   elm_object_part_text_set(sd->layout, "elm.text.cursor_pxy", buf);
+
+   snprintf(buf, sizeof(buf), "%0.2f", rel_x);
+   elm_object_part_text_set(sd->layout, "elm.text.cursor_relx", buf);
+   snprintf(buf, sizeof(buf), "%0.2f", rel_y);
+   elm_object_part_text_set(sd->layout, "elm.text.cursor_rely", buf);
 }
