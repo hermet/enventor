29c54814c37f9041e1999b891db78f519aeb4a5a
ChunEon Park <hermet@hermet.pe.kr>
2014-10-19 14:43:07 +0900

    main: remove workaround code that is no more valid.

--- src/bin/main.c
+++ src/bin/main.c
@@ -442,8 +442,14 @@ enventor_setup(app_data *ad)
    evas_object_smart_callback_add(enventor, "ctxpopup,selected",
                                   enventor_ctxpopup_selected_cb, ad);
 
-   /* FIXME: Current envnetor object needs resource path set, file set and extra
-	  	options in a order. */
+   enventor_object_font_scale_set(enventor, config_font_scale_get());
+   enventor_object_live_view_scale_set(enventor, config_view_scale_get());
+   enventor_object_linenumber_set(enventor, config_linenumber_get());
+   enventor_object_part_highlight_set(enventor, config_part_highlight_get());
+   enventor_object_auto_indent_set(enventor, config_auto_indent_get());
+   enventor_object_auto_complete_set(enventor, config_auto_complete_get());
+   enventor_object_dummy_swallow_set(enventor, config_dummy_swallow_get());
+
    Eina_List *list = eina_list_append(NULL, config_edj_path_get());
    enventor_object_path_set(enventor, ENVENTOR_OUT_EDJ, list);
    eina_list_free(list);
@@ -457,14 +463,6 @@ enventor_setup(app_data *ad)
    enventor_object_path_set(enventor, ENVENTOR_RES_DATA,
                             config_edc_dat_path_list_get());
    enventor_object_file_set(enventor, config_edc_path_get());
-
-   enventor_object_font_scale_set(enventor, config_font_scale_get());
-   enventor_object_live_view_scale_set(enventor, config_view_scale_get());
-   enventor_object_linenumber_set(enventor, config_linenumber_get());
-   enventor_object_part_highlight_set(enventor, config_part_highlight_get());
-   enventor_object_auto_indent_set(enventor, config_auto_indent_get());
-   enventor_object_auto_complete_set(enventor, config_auto_complete_get());
-   enventor_object_dummy_swallow_set(enventor, config_dummy_swallow_get());
 
    evas_object_size_hint_expand_set(enventor, EVAS_HINT_EXPAND,
                                     EVAS_HINT_EXPAND);
