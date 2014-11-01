22a39f2d47a8b4e1acd8351e7d82fbe3f8377ab8
ChunEon Park <hermet@hermet.pe.kr>
2014-11-01 18:31:27 +0900

    app/file_mgr: update missed function delcares.

--- src/include/file_mgr.h
+++ src/include/file_mgr.h
@@ -1,4 +1,7 @@
 Eina_Bool file_mgr_warning_is_opened(void);
 void file_mgr_warning_close(void);
+void file_mgr_warning_open(void);
 void file_mgr_init(Evas_Object *enventor);
 void file_mgr_term(void);
+int file_mgr_edc_modified_get(void);
+void file_mgr_reset(void);
