2e541e722ab4c2256790ea114c3a8f8db7673b12
Jaehyun Cho <jae_hyun.cho@samsung.com>
2015-12-07 17:17:29 +0900

    enventor: Fix to move scroller bar to correct position for find function
    enventor creates a scroller instead of using entry's scroller for text
    editor. This causes that the scroller bar moves to incorrect position
    when a word is selected by find function. To resolve this problem, set
    entry's cursor position before setting entry's select region.

--- src/lib/enventor_smart.c
+++ src/lib/enventor_smart.c
@@ -536,6 +536,7 @@ _enventor_object_select_region_set(Eo *obj EINA_UNUSED,
                                    Enventor_Object_Data *pd, int start, int end)
 {
    edit_selection_clear(pd->ed);
+   elm_entry_cursor_pos_set(edit_entry_get(pd->ed), start);
    elm_entry_select_region_set(edit_entry_get(pd->ed), start, end);
 }
 
