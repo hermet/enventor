3b4a6767a58163b934fab19f0578795d861b7793
Mykyta Biliavskyi <m.biliavskyi@samsung.com>
2015-12-16 17:29:55 +0900

    Dummy: Support dummy spacer like dummy swallow.
    Create dummy object for all spacer in the group.
    This feature Enable/Disable by the button on
    tools panel "Dummy Swallow" or by hotkey Ctrl+W.

--- src/lib/dummy_obj.c
+++ src/lib/dummy_obj.c
@@ -18,6 +18,7 @@ typedef struct dummy_obj_s
 {
    Evas_Object *layout;
    Eina_List *swallows;
+   Eina_List *spacers;
    Ecore_Animator *animator;
 } dummy_obj;
 
@@ -33,7 +34,7 @@ dummy_objs_update(dummy_obj *dummy)
    Eina_List *parts = edje_edit_parts_list_get(dummy->layout);
    Eina_List *l, *l_next, *l2;
    char *part_name;
-   Edje_Part_Type type;
+   Edje_Part_Type type = EDJE_PART_TYPE_NONE;
    part_obj *po;
    Evas *evas = evas_object_evas_get(dummy->layout);
    Eina_Bool removed;
@@ -49,8 +50,9 @@ dummy_objs_update(dummy_obj *dummy)
              if (strlen(po->name) != strlen(part_name)) continue;
              if (!strcmp(po->name, part_name))
                {
-                  if (edje_edit_part_type_get(dummy->layout, part_name) ==
-                      EDJE_PART_TYPE_SWALLOW)
+                  type = edje_edit_part_type_get(dummy->layout, part_name);
+                  if ((type == EDJE_PART_TYPE_SWALLOW) ||
+                       (type == EDJE_PART_TYPE_SPACER))
                     removed = EINA_FALSE;
                   break;
                }
@@ -59,7 +61,10 @@ dummy_objs_update(dummy_obj *dummy)
           {
              evas_object_del(po->obj);
              eina_stringshare_del(po->name);
-             dummy->swallows = eina_list_remove_list(dummy->swallows, l);
+             if (type == EDJE_PART_TYPE_SWALLOW)
+               dummy->swallows = eina_list_remove_list(dummy->swallows, l);
+             else if (type == EDJE_PART_TYPE_SPACER)
+               dummy->spacers = eina_list_remove_list(dummy->spacers, l);
              free(po);
           }
      }
@@ -91,11 +96,67 @@ dummy_objs_update(dummy_obj *dummy)
              po->name = eina_stringshare_add(part_name);
              dummy->swallows = eina_list_append(dummy->swallows, po);
           }
+        else if (type == EDJE_PART_TYPE_SPACER)
+          {
+             Eina_List *spacer_l;
+             Evas_Object *obj = NULL;
+             int x = 0, y = 0, w = 0, h = 0, lx = 0, ly = 0;
+
+             EINA_LIST_FOREACH(dummy->spacers, spacer_l, po)
+                if (po->name == part_name)
+                  {
+                     obj = po->obj;
+                     break;
+                  }
+             if (!obj)
+               {
+                  Evas_Object *scroller = view_obj_get(VIEW_DATA);
+                  Evas_Object *scroller_edje = elm_layout_edje_get(scroller);
+                  Evas_Object *clipper =
+                     (Evas_Object *)edje_object_part_object_get(scroller_edje,
+                                                                "clipper");
+                  obj = elm_layout_add(scroller);
+                  elm_layout_file_set(obj, EDJE_PATH, "spacer");
+                  evas_object_smart_member_add(obj, scroller);
+
+
+                  po = malloc(sizeof(part_obj));
+                  po->obj = obj;
+                  po->name = eina_stringshare_add(part_name);
+                  dummy->spacers = eina_list_append(dummy->spacers, po);
+                  evas_object_show(obj);
+                  evas_object_clip_set(obj, clipper);
+               }
+             evas_object_geometry_get(dummy->layout, &lx, &ly, NULL, NULL);
+             edje_object_part_geometry_get(dummy->layout, part_name, &x, &y, &w, &h);
+             evas_object_resize(obj, w, h);
+             evas_object_move(obj, lx + x, ly + y);
+          }
      }
 
    edje_edit_string_list_free(parts);
 }
 
+static void
+layout_geom_changed_cb(void *data, Evas *evas EINA_UNUSED,
+                       Evas_Object *obj, void *ei EINA_UNUSED)
+{
+   dummy_obj *dummy = (dummy_obj *)data;
+
+   Eina_List *spacer_l;
+   part_obj *po;
+   int x = 0, y = 0, w = 0, h = 0, lx = 0, ly = 0;
+
+   evas_object_geometry_get(obj, &lx, &ly, NULL, NULL);
+
+   EINA_LIST_FOREACH(dummy->spacers, spacer_l, po)
+     if (edje_object_part_exists(obj, po->name))
+       {
+          edje_object_part_geometry_get(obj, po->name, &x, &y, &w, &h);
+          evas_object_resize(po->obj, w, h);
+          evas_object_move(po->obj, lx + x, ly + y);
+       }
+}
 static Eina_Bool
 animator_cb(void *data)
 {
@@ -143,6 +204,10 @@ dummy_obj_new(Evas_Object *layout)
    evas_object_event_callback_add(layout, EVAS_CALLBACK_DEL, layout_del_cb,
                                   dummy);
 
+   evas_object_event_callback_add(layout, EVAS_CALLBACK_RESIZE,
+                                  layout_geom_changed_cb, dummy);
+   evas_object_event_callback_add(layout, EVAS_CALLBACK_MOVE,
+                                  layout_geom_changed_cb, dummy);
    dummy->layout = layout;
    dummy->animator = animator;
 }
@@ -153,8 +218,20 @@ dummy_obj_del(Evas_Object *layout)
    dummy_obj *dummy = evas_object_data_get(layout, DUMMYOBJ);
    if (!dummy) return;
 
+   evas_object_event_callback_del_full(layout, EVAS_CALLBACK_RESIZE,
+                                  layout_geom_changed_cb, dummy);
+   evas_object_event_callback_del_full(layout, EVAS_CALLBACK_MOVE,
+                                  layout_geom_changed_cb, dummy);
+
    part_obj *po;
    EINA_LIST_FREE(dummy->swallows, po)
+     {
+        evas_object_del(po->obj);
+        eina_stringshare_del(po->name);
+        free(po);
+     }
+
+   EINA_LIST_FREE(dummy->spacers, po)
      {
         evas_object_del(po->obj);
         eina_stringshare_del(po->name);
