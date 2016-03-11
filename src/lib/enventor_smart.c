986663f5ed27d21ab3a309f6db5895ef1892a7cd
Hermet Park <hermet@hermet.pe.kr>
2016-03-11 17:38:36 +0900

    fix the build break that introduced by eo syntax change.

--- src/lib/enventor_smart.c
+++ src/lib/enventor_smart.c
@@ -751,7 +751,8 @@ EAPI Evas_Object *
 enventor_object_add(Evas_Object *parent)
 {
    EINA_SAFETY_ON_NULL_RETURN_VAL(parent, NULL);
-   Evas_Object *obj = eo_add(MY_CLASS, parent);
+   Evas_Object *obj;
+   eo_add(&obj, MY_CLASS, parent);
    return obj;
 }
 
