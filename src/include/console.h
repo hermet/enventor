c139a34f6a7e5f01b63a3a061d48783cfed4b0d9
Hermet Park <hermet@hermet.pe.kr>
2016-04-18 16:47:44 +0900

    console: arrange default console height.

--- src/include/console.h
+++ src/include/console.h
@@ -1,4 +1,4 @@
-#define DEFAULT_CONSOLE_SIZE 0.175
+#define DEFAULT_CONSOLE_SIZE 0.135
 
 Evas_Object *console_create(Evas_Object *parent);
 void console_text_set(Evas_Object *console, const char *text);
