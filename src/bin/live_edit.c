1c21a8538ff0a445fb8f6380685e2418ac1e8280
Hermet Park <hermet@hermet.pe.kr>
2016-04-27 00:03:25 +0900

    status: adjust stats msg idle time.
    as well as some messages.

--- src/bin/live_edit.c
+++ src/bin/live_edit.c
@@ -1010,7 +1010,7 @@ live_btn_clicked_cb(void *data, Evas_Object *obj EINA_UNUSED,
 
    live_edit_layer_set(ld);
 
-   stats_info_msg_update(_("Double click the part to confirm. (Esc = cancel)"));
+   stats_info_msg_update(_("Double click part to confirm. (Esc = cancel)"));
 }
 
 static Evas_Object *
