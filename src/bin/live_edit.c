3a6d844df21d3419652fec094951c0ce4c8ede49
Hermet Park <hermet@hermet.pe.kr>
2016-05-28 19:37:31 +0900

    live_edit: update message.

--- src/bin/live_edit.c
+++ src/bin/live_edit.c
@@ -2118,7 +2118,7 @@ live_btn_clicked_cb(void *data, Evas_Object *obj EINA_UNUSED,
 
    live_edit_layer_set(ld);
 
-   stats_info_msg_update(_("Double click part to confirm. (Esc = cancel, Direction Key = move item per pixel)"));
+   stats_info_msg_update(_("Double click part to confirm. (Esc = cancel, Direction Key = move item per pixel, Ctrl = disable auto-aligning)"));
 }
 
 static Evas_Object *
