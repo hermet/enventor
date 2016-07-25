09ea8541bb77ed0e28ab59c6b696f5f971532b2c
Bowon Ryu <bowon.ryu@samsung.com>
2016-07-25 16:02:08 +0900

    help: updated help
    Test Plan:
    1. enventor
    2. press F1 Key
    
    Reviewers: Jaehyun_Cho, Hermet
    
    Differential Revision: https://phab.enlightenment.org/D4178

--- src/bin/help.c
+++ src/bin/help.c
@@ -4,6 +4,14 @@
 
 #include "common.h"
 
+typedef struct help_data_s
+{
+   Evas_Object *list;
+   Evas_Object *box;
+   Evas_Object *button;
+} help_data;
+
+static help_data *g_hd = NULL;
 static Evas_Coord win_x = -1;
 static Evas_Coord win_y = -1;
 static Evas_Coord win_w = DEFAULT_HELP_WIN_W;
@@ -39,6 +47,86 @@ win_moved_cb(void *data EINA_UNUSED, Evas_Object *obj,
    evas_object_smart_callback_del(obj, "moved", win_moved_cb);
 }
 
+static void
+list_item_selected_cb(void *data, Evas_Object *obj, void *event_info)
+{
+   Evas_Object *label;
+   Evas_Object *entry;
+   char buf[PATH_MAX];
+   help_data *hd = g_hd;
+   char *item = data;
+
+   elm_box_clear(hd->box);
+
+   //Label
+   label =  elm_label_add(hd->box);
+   evas_object_size_hint_align_set(label, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_box_pack_end(hd->box, label);
+   evas_object_show(label);
+
+   //Entry
+   entry = elm_entry_add(hd->box);
+   elm_entry_scrollable_set(entry, EINA_TRUE);
+   elm_entry_line_wrap_set(entry, EINA_TRUE);
+   elm_entry_editable_set(entry, EINA_FALSE);
+   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
+   evas_object_size_hint_weight_set(entry, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(entry, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_box_pack_end(hd->box, entry);
+   evas_object_show(entry);
+
+   //Read File
+   if (!strcmp(item, "about"))
+   {
+      elm_object_text_set(label, "<font_size=11><b>About</b></font_size>");
+      snprintf(buf, sizeof(buf), "%s/help/INTRO", elm_app_data_dir_get());
+      elm_entry_autosave_set(entry, EINA_FALSE);
+      elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   }
+   else if (!strcmp(item, "history"))
+   {
+      elm_object_text_set(label, "<font_size=11><b>Version History</b></font_size>");
+      snprintf(buf, sizeof(buf), "%s/help/HISTORY", elm_app_data_dir_get());
+      elm_entry_autosave_set(entry, EINA_FALSE);
+      elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   }
+   else if (!strcmp(item, "shortcut"))
+   {
+      elm_object_text_set(label, "<font_size=11><b>Shortcut Keys</b></font_size>");
+      snprintf(buf, sizeof(buf), "%s/help/SHORTCUT", elm_app_data_dir_get());
+      elm_entry_autosave_set(entry, EINA_FALSE);
+      elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   }
+   else if (!strcmp(item, "command"))
+   {
+      elm_object_text_set(label, "<font_size=11><b>Commandline Usage</b></font_size>");
+      snprintf(buf, sizeof(buf), "%s/help/COMMAND", elm_app_data_dir_get());
+      elm_entry_autosave_set(entry, EINA_FALSE);
+      elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   }
+   else if (!strcmp(item, "devel"))
+   {
+      elm_object_text_set(label, "<font_size=11><b>Developers</b></font_size>");
+      snprintf(buf, sizeof(buf), "%s/help/DEVEL", elm_app_data_dir_get());
+      elm_entry_autosave_set(entry, EINA_FALSE);
+      elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   }
+
+   //Back Button
+   evas_object_show(hd->button);
+}
+
+static void
+backbutton_clicked_cb(void *data, Evas_Object *obj, void *event_info)
+{
+   help_data *hd = g_hd;
+   Evas_Object *box = data;
+   elm_box_clear(box);
+
+   elm_list_item_selected_set(elm_list_selected_item_get(hd->list), EINA_FALSE);
+   evas_object_hide(hd->button);
+}
+
 /*****************************************************************************/
 /* Externally accessible calls                                               */
 /*****************************************************************************/
@@ -53,162 +141,96 @@ help_open(void)
       return;
    }
 
-   Evas_Object *entry;
+   help_data *hd = malloc(sizeof(help_data));
+   if (!hd)
+   {
+      mem_fail_msg();
+      return ;
+   }
+   g_hd = hd;
+
    char buf[PATH_MAX];
 
    //Win
    win = elm_win_add(base_win_get(), _("Enventor Help"), ELM_WIN_BASIC);
    elm_win_focus_highlight_enabled_set(win, EINA_TRUE);
-   elm_win_title_set(win, _("Help"));
+   elm_win_title_set(win, _("About Enventor"));
    evas_object_smart_callback_add(win, "delete,request", win_delete_request_cb,
                                   NULL);
    evas_object_smart_callback_add(win, "moved", win_moved_cb, NULL);
 
    //Bg
    Evas_Object *bg = elm_bg_add(win);
+   evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_show(bg);
    elm_win_resize_object_add(win, bg);
 
-   //Scroller
-   Evas_Object *scroller = elm_scroller_add(win);
-   evas_object_show(scroller);
-   elm_win_resize_object_add(win, scroller);
-
    //Box
    Evas_Object *box = elm_box_add(win);
-   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(box, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_weight_set(box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, box);
    evas_object_show(box);
-   elm_object_content_set(scroller, box);
 
-   //Frame1: Introduction
-   Evas_Object *intro_frame = elm_frame_add(box);
-   elm_frame_autocollapse_set(intro_frame, EINA_TRUE);
-   elm_object_text_set(intro_frame, "About");
-   evas_object_size_hint_weight_set(intro_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(intro_frame, EVAS_HINT_FILL, 0);
-   evas_object_show(intro_frame);
-   elm_box_pack_end(box, intro_frame);
+   //Title Bg
+   Evas_Object *title_bg = elm_bg_add(box);
+   snprintf(buf, sizeof(buf), "%s/images/ENVENTOR_EMBEDDED_FULL_LOGO.png",
+            elm_app_data_dir_get());
+   elm_bg_file_set(title_bg, buf, NULL);
+   evas_object_size_hint_align_set(title_bg, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_size_hint_min_set(title_bg, 430, 100);
+   elm_box_pack_end(box, title_bg);
+   evas_object_show(title_bg);
 
-   //Entry
-   entry = elm_entry_add(intro_frame);
-   elm_entry_scrollable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, EINA_TRUE);
-   elm_entry_editable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
-   elm_object_content_set(intro_frame, entry);
+   //Version Label
+   Evas_Object *label = elm_label_add(title_bg);
+   elm_object_text_set(label, "<align=0.95>Enventor v0.9.0</align>");
+   elm_object_content_set(title_bg, label);
+   evas_object_show(label);
 
-   //Read File
-   snprintf(buf, sizeof(buf), "%s/help/INTRO", elm_app_data_dir_get());
-   elm_entry_autosave_set(entry, EINA_FALSE);
-   elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
+   //Entry Box
+   Evas_Object *entry_box = elm_box_add(win);
+   evas_object_size_hint_weight_set(entry_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, entry_box);
+   evas_object_show(entry_box);
+   hd->box = entry_box;
 
+   //List
+   Evas_Object *list = elm_list_add(box);
+   evas_object_size_hint_weight_set(list, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(list, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   elm_object_tree_focus_allow_set(list, EINA_FALSE);
+   elm_list_select_mode_set(list, ELM_OBJECT_SELECT_MODE_DEFAULT);
+   evas_object_show(list);
+   elm_box_pack_end(box, list);
+   hd->list = list;
 
-   //Frame2: Version History
-   Evas_Object *version_frame = elm_frame_add(box);
-   elm_frame_autocollapse_set(version_frame, EINA_TRUE);
-   elm_frame_collapse_set(version_frame, EINA_TRUE);
-   elm_object_text_set(version_frame, "Version History");
-   evas_object_size_hint_weight_set(version_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(version_frame, EVAS_HINT_FILL,
-                                   0);
-   evas_object_show(version_frame);
-   elm_box_pack_end(box, version_frame);
+   elm_list_item_append(list, "About", NULL, NULL, list_item_selected_cb,
+                        "about");
+   elm_list_item_append(list, "Version History", NULL, NULL, list_item_selected_cb,
+                        "history");
+   elm_list_item_append(list, "Shortcut Keys", NULL, NULL, list_item_selected_cb,
+                        "shortcut");
+   elm_list_item_append(list, "Commandline Usage", NULL, NULL, list_item_selected_cb,
+                        "command");
+   elm_list_item_append(list, "Developers", NULL, NULL, list_item_selected_cb,
+                        "devel");
 
-   //Entry
-   entry = elm_entry_add(version_frame);
-   elm_entry_scrollable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, EINA_TRUE);
-   elm_entry_editable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
-   elm_object_content_set(version_frame, entry);
+   // Back Button Box
+   Evas_Object *button_box = elm_box_add(win);
+   evas_object_size_hint_weight_set(button_box, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_win_resize_object_add(win, button_box);
+   evas_object_show(button_box);
 
-   //Read File
-   snprintf(buf, sizeof(buf), "%s/help/HISTORY", elm_app_data_dir_get());
-   elm_entry_autosave_set(entry, EINA_FALSE);
-   elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
-
-
-   //Frame3: Shortcut Keys
-   Evas_Object *key_frame = elm_frame_add(box);
-   elm_frame_autocollapse_set(key_frame, EINA_TRUE);
-   elm_frame_collapse_set(key_frame, EINA_TRUE);
-   elm_object_text_set(key_frame, "Shortcut keys");
-   evas_object_size_hint_weight_set(key_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(key_frame, EVAS_HINT_FILL, 0);
-   evas_object_show(key_frame);
-   elm_box_pack_end(box, key_frame);
-
-   //Entry
-   entry = elm_entry_add(key_frame);
-   elm_entry_scrollable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, EINA_TRUE);
-   elm_entry_editable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
-   elm_object_content_set(key_frame, entry);
-
-   //Read File
-   snprintf(buf, sizeof(buf), "%s/help/SHORTCUT", elm_app_data_dir_get());
-   elm_entry_autosave_set(entry, EINA_FALSE);
-   elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
-
-
-   //Frame4: Commandline
-   Evas_Object *command_frame = elm_frame_add(box);
-   elm_frame_autocollapse_set(command_frame, EINA_TRUE);
-   elm_frame_collapse_set(command_frame, EINA_TRUE);
-   elm_object_text_set(command_frame, "Commandline Usage");
-   evas_object_size_hint_weight_set(command_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(command_frame, EVAS_HINT_FILL, 0);
-   evas_object_show(command_frame);
-   elm_box_pack_end(box, command_frame);
-
-   //Entry
-   entry = elm_entry_add(command_frame);
-   elm_entry_scrollable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, EINA_TRUE);
-   elm_entry_editable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
-   elm_object_content_set(command_frame, entry);
-
-   //Read File
-   snprintf(buf, sizeof(buf), "%s/help/COMMAND", elm_app_data_dir_get());
-   elm_entry_autosave_set(entry, EINA_FALSE);
-   elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
-
-   //Frame5: EDC References
-   Evas_Object *refer_frame = elm_frame_add(box);
-   elm_object_text_set(refer_frame, "EDC References (Not support yet)");
-   evas_object_size_hint_weight_set(refer_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(refer_frame, EVAS_HINT_FILL,
-                                   EVAS_HINT_FILL);
-   evas_object_show(refer_frame);
-   elm_box_pack_end(box, refer_frame);
-
-   //Frame6:Developers
-   Evas_Object *devel_frame = elm_frame_add(box);
-   elm_frame_autocollapse_set(devel_frame, EINA_TRUE);
-   elm_frame_collapse_set(devel_frame, EINA_TRUE);
-   elm_object_text_set(devel_frame, "Developers");
-   evas_object_size_hint_weight_set(devel_frame, EVAS_HINT_EXPAND, 0);
-   evas_object_size_hint_align_set(devel_frame, EVAS_HINT_FILL,
-                                   EVAS_HINT_FILL);
-   evas_object_show(devel_frame);
-   elm_box_pack_end(box, devel_frame);
-
-   //Entry
-   entry = elm_entry_add(devel_frame);
-   elm_entry_scrollable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, EINA_TRUE);
-   elm_entry_editable_set(entry, EINA_FALSE);
-   elm_entry_line_wrap_set(entry, ELM_WRAP_MIXED);
-   elm_object_content_set(devel_frame, entry);
-
-   //Read File
-   snprintf(buf, sizeof(buf), "%s/help/DEVEL", elm_app_data_dir_get());
-   elm_entry_autosave_set(entry, EINA_FALSE);
-   elm_entry_file_set(entry, buf, ELM_TEXT_FORMAT_MARKUP_UTF8);
-
+   //Back Button
+   Evas_Object *button = elm_button_add(button_box);
+   evas_object_size_hint_weight_set(button, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_size_hint_align_set(button, 0.95, 0.95);
+   evas_object_size_hint_min_set(button, 60, 30);
+   elm_object_text_set(button, "Back");
+   evas_object_smart_callback_add(button, "clicked", backbutton_clicked_cb, entry_box);
+   elm_box_pack_end(button_box, button);
+   evas_object_hide(button);
+   hd->button = button;
 
    win_w = (Evas_Coord) ((double) win_w * elm_config_scale_get());
    win_h = (Evas_Coord) ((double) win_h * elm_config_scale_get());
@@ -231,10 +253,11 @@ help_close(void)
 {
    Evas_Object *win = g_win;
    if (!win) return;
+   free(g_hd);
 
    //Save last state
    evas_object_geometry_get(win, NULL, NULL, &win_w, &win_h);
    elm_win_screen_position_get(win, &win_x, &win_y);
-   evas_object_del(win);
+   evas_object_del(win);   
    g_win = NULL;
 }
--- src/include/help.h
+++ src/include/help.h
@@ -1,5 +1,5 @@
-#define DEFAULT_HELP_WIN_W 533
-#define DEFAULT_HELP_WIN_H 533
+#define DEFAULT_HELP_WIN_W 430
+#define DEFAULT_HELP_WIN_H 285
 
 void help_open(void);
 void help_close(void);
