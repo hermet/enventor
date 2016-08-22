30c32488e038e0962ed6708359fc286f1477adb8
Hermet Park <hermet@hermet.pe.kr>
2016-08-23 08:53:37 +0900

    live_edit: multiply base_scale to the live edit object.
    Previously, live edit object doesn't have base scale value
    so text and textblock size is incorrect.
    
    Now, its fixed.

--- src/bin/live_edit.c
+++ src/bin/live_edit.c
@@ -358,7 +358,8 @@ live_edit_symbol_set(live_data *ld)
    Evas_Object *layout_symbol = elm_layout_add(ld->layout);
    elm_layout_file_set(layout_symbol, EDJE_PATH, buf);
    elm_object_scale_set(layout_symbol,
-                        enventor_object_live_view_scale_get(base_enventor_get()));
+                     (1/enventor_object_base_scale_get(base_enventor_get())) *
+                     enventor_object_live_view_scale_get(base_enventor_get()));
    elm_object_part_content_set(ld->layout, "elm.swallow.symbol", layout_symbol);
 }
 
